584047d48a742ba1871f6f67a9b4ad015a364420
Faisal Rashid <faisalrashid@LIVE.IN>
2019-04-07 21:08:25 +0530

    Made 2 player mode unbeatable

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,4 +1,4 @@
-
+// var automate = false;
 var currentPlayer = "X";
 
 var checkedBoxes = [];
@@ -7,13 +7,31 @@ var turnCount = 0;
 
 var gameMode = 'PvP';
 
-var model = getModel();
+// var model = getModel();
 
 document.querySelectorAll('.box').forEach((value, key) => {
     value.addEventListener("click", () => {
         onCheckBox(value);
     });
 });
+
+// function automateGame(){
+//     if(turnCount == 0){
+//         var move = computeRandomMove();
+
+//         var el  = document.querySelector(`[id='${move}']`);
+    
+//         onCheckBox(el);
+    
+//     }
+
+//     else{
+//         computerPlays();
+
+//     }
+
+
+// }
 
 function onGameModeChange(mode, _el) {
     if (_el.classList.contains('mode-selected'))
@@ -38,9 +56,18 @@ function onCheckBox(element) {
     element.disabled = "disabled";
     turnCount++;
     var gameStatus = checkWinner();
+    // if(gameStatus != 'game on' && automate == true){
+    //     newGame();
+    //     automateGame();
+    // }
     switchPlayer();
-    if (turnCount % 2 == 1 && gameStatus != 'game over' && gameMode == "PvC")
+    if (turnCount % 2 == 1 && gameStatus != 'game over' && gameStatus != 'game drawn' && gameMode == "PvC"){
         computerPlays();
+        // if(automate == true)
+        // automateGame();
+    }
+        
+
 }
 
 function onUncheckBox(element, isImplicit = false) {
@@ -51,9 +78,6 @@ function onUncheckBox(element, isImplicit = false) {
         turnCount--;
         switchPlayer();
     }
-
-
-
 }
 
 function switchPlayer() {
@@ -122,7 +146,7 @@ function calculateScore(positions, isCheckOnly) {
     if (positions.length == 5) {
         if (!isCheckOnly)
             showWinner(true);
-        return 'game over';
+        return 'game drawn';
     }
     return 'game on';
 }
@@ -142,7 +166,7 @@ function showWinner(noWinner = false) {
         document.querySelector('.winner-screen .body').innerHTML = 'Its a Draw!';
         document.querySelector('.winner-screen').classList.toggle('fade-in');
         document.querySelector('.winner-screen').classList.toggle('fade-out');
-        updateModel('draw');
+        //updateModel('draw');
         return;
     }
     else {
@@ -151,7 +175,7 @@ function showWinner(noWinner = false) {
         document.querySelector('.winner-screen').classList.toggle('fade-in');
         document.querySelector('.winner-screen').classList.toggle('fade-out');
         document.querySelector('#score-' + currentPlayer).textContent = Number(document.querySelector('#score-' + currentPlayer).textContent) + 1;
-        updateModel('win');
+        //updateModel('win');
         return;
     }
 }
@@ -173,22 +197,30 @@ function newGame() {
 }
 
 function computerPlays() {
-
+    var nextBoxCoords;
     //debugger;
-    var nextBoxCoords = computeFinishingMove();
+
+    if(turnCount == 1){
+        nextBoxCoords = computeFirstMove();
+    }
+    if (!nextBoxCoords){
+        nextBoxCoords = computeFinishingMove();
+    }
 
     if (!nextBoxCoords) {
         nextBoxCoords = computeSavingMove();
     }
+    if (!nextBoxCoords)
+        nextBoxCoords = predictTrappingMove();
     
-    if (model.length) {
-        if (!nextBoxCoords)
-            nextBoxCoords = computeWinningMove();
+    // if (model.length) {
+    //     if (!nextBoxCoords)
+    //         nextBoxCoords = computeWinningMove();
 
-        if (!nextBoxCoords) {
-            nextBoxCoords = computeDrawMove();
-        }
-    }
+    //     if (!nextBoxCoords) {
+    //         nextBoxCoords = computeDrawMove();
+    //     }
+    // }
     if (!nextBoxCoords) {
         nextBoxCoords = computeRandomMove();
     }
@@ -197,106 +229,28 @@ function computerPlays() {
     onCheckBox(nextBox);
 }
 
-function computeWinningMove() {
-    var checkedBoxesSimplified = checkedBoxes.map(b => b.box);
-    var winningCombinations = model.filter(m => m.result == 'win'
-        && m.winner == (turnCount + 1) % 2
-        && JSON.stringify(m.pattern.slice(0, turnCount)) == JSON.stringify(checkedBoxesSimplified))
-        .sort();
-
-    var winningCount = 0;
-    var currentCount = 0;
-    var winningCombination;
-
-    for (var i = 0; i < winningCombinations.length; i++) {
-        if (isLosingCombination(winningCombinations[i])) {
-            continue;
-        }
-        if (i == 0 || winningCombinations[i - 1] == winningCombinations[i]) {
-            currentCount++;
-        }
-        if (currentCount > winningCount) {
-            winningCount = currentCount;
-            winningCombination = winningCombinations[i];
-        }
-    }
-
-    if (winningCombination){
-        console.log('Playing Winning Move');
-        return winningCombination.pattern[turnCount];
-    }
-    else
-        return '';
+function computeFirstMove(){
+    var playedMove = checkedBoxes.map(b => b.box)[0];
+    var edgeMoves = ['0-1', '1-0', '1-2', '2-1'];
+    var cornerMoves = ['0-0', '0-2', '2-0', '2-2'];
+    var centerMove = ['1-1'];
+    if(edgeMoves.find(m => m == playedMove))
+        return edgeMoveResponse(playedMove);
+    else if(cornerMoves.find(m => m == playedMove))
+        return '1-1';
+    else if(centerMove.find(m => m == playedMove))
+        return cornerMoves[Math.floor(Math.random()*cornerMoves.length)];
 }
 
-function computeDrawMove() {
-    var checkedBoxesSimplified = checkedBoxes.map(b => b.box);
-    var drawingCombinations = model.filter(m => m.result == 'draw'
-        && m.winner == -1
-        && JSON.stringify(m.pattern.slice(0, turnCount)) == JSON.stringify(checkedBoxesSimplified))
-        .sort();
-    
-    if(!drawingCombinations)
-        return '';
-
-    var drawingCount = 0;
-    var currentCount = 0;
-    var drawingCombination;
-
-    for (var i = 0; i < drawingCombinations.length; i++) {
-        if (isLosingCombination(drawingCombinations[i]))
-            continue;
-        if (i == 0 || drawingCombinations[i - 1] == drawingCombinations[i]) {
-            currentCount++;
-        }
-        if (currentCount > drawingCount) {
-            drawingCount = currentCount;
-            drawingCombination = drawingCombinations[i];
-        }
-    }
-    
-    if (drawingCombination){
-        console.log('Playing Drawing Move');
-        return drawingCombination.pattern[turnCount];
-    }
-    else
-        return '';
-}
-
-function computeRandomMove() {
-    var remainingMoves = getRemainingMoves();
-    var movesTried = 0;
-    var randomMove;
-    while(true)
-    {
-        console.log('Trying Random Move!');
-        randomMove = remainingMoves[Math.floor(Math.random() * remainingMoves.length)];
-        remainingMoves = remainingMoves.filter(m => m != randomMove);
-        movesTried++;
-        if(!isLosingMove(randomMove)){
-            console.log('Playing Calculated Random Move');
-            return randomMove;
-        }
-        else if (isLosingMove(randomMove) && movesTried != remainingMoves.length){
-            console.log('Playing Any Random Move');
-            return getRemainingMoves()[Math.floor(Math.random() * remainingMoves.length)];
-        }
-    }
-}
-
-function isLosingCombination(combination) {
-    //debugger;
-    return !!model.find(m => m.result == 'win'
-        && m.winner == (turnCount + 2) % 2 // Check Losing Combination
-        && JSON.stringify(m.pattern.slice(0, turnCount+1)) == JSON.stringify(combination.pattern.slice(0, turnCount+1)))
-}
-
-function isLosingMove(move){
-    var playedMoves = checkedBoxes.map(b => b.box);
-    var appendedPlayedMoves = playedMoves.push(move);
-    return !!model.find(m => m.result == 'win'
-                    && m.winner == (turnCount + 2) % 2 // Check Losing Combination
-                    && JSON.stringify(m.pattern.slice(0, turnCount+1)) == JSON.stringify(appendedPlayedMoves))
+function edgeMoveResponse(playedMove){
+    if(playedMove == '1-2') 
+        return '0-2';
+    else if (playedMove == "0-1") 
+        return "0-0";
+    else if (playedMove == "1-0") 
+        return "2-0";
+    else if(playedMove == '2-1') 
+        return '2-0';
 }
 
 function computeSavingMove() {
@@ -343,6 +297,187 @@ function computeFinishingMove() {
     
 }
 
+function predictTrappingMove() {
+    var checkedBoxesBackup = checkedBoxes.slice();
+    var remainingMoves = getRemainingMoves();
+    var nextMove;
+    var moveFound;
+    for(var move of remainingMoves){
+        checkedBoxes.push({box: move, player: currentPlayer})
+        switchPlayer();
+
+        //Check if the opponent needs to play a saving move
+
+        var savingMove =  computeSavingMove();
+        if(savingMove){
+            checkedBoxes.push({box: savingMove, player: currentPlayer});
+            if(checkTrap() == 'no trap'){
+                checkedBoxes.pop();
+                switchPlayer();
+                nextMove = move;
+                break;
+            }
+            checkedBoxes.pop();
+            switchPlayer();
+            continue;
+        }
+
+        //If no saving move is required, check each position
+        else{
+            switchPlayer();
+            for(var opponentMove of getRemainingMoves()){
+                switchPlayer();
+                moveFound = true;
+                
+                checkedBoxes.push({box: opponentMove, player: currentPlayer});
+                if(checkTrap() == 'trapped'){
+                    moveFound = false;
+                    checkedBoxes.pop();
+                    switchPlayer();
+                    break;
+                }
+                checkedBoxes.pop();
+                switchPlayer();
+            }
+        }
+
+        checkedBoxes.pop();
+        if(moveFound){
+            nextMove = move;
+            break;
+        }
+    }
+    checkedBoxes = checkedBoxesBackup;
+    return nextMove;
+}
+
+function checkTrap(){
+
+    var boxes = getRemainingMoves();
+    var winningMoveCount = 0;
+    for(var freeMove of boxes){
+        checkedBoxes.push({box: freeMove, player: currentPlayer});
+        var result = checkWinner(true);
+        if(result == 'game over')
+            winningMoveCount++
+        checkedBoxes.pop();
+    }
+    if(winningMoveCount > 1){
+        return 'trapped';
+    }
+    else{
+        return 'no trap';
+    }
+}
+
+// function computeWinningMove() {
+//     var checkedBoxesSimplified = checkedBoxes.map(b => b.box);
+//     var winningCombinations = model.filter(m => m.result == 'win'
+//         && m.winner == (turnCount + 1) % 2
+//         && JSON.stringify(m.pattern.slice(0, turnCount)) == JSON.stringify(checkedBoxesSimplified))
+//         .sort();
+
+//     var winningCount = 0;
+//     var currentCount = 0;
+//     var winningCombination;
+    
+//     for (var i = 0; i < winningCombinations.length; i++) {
+//         if (isLosingCombination(winningCombinations[i])) {
+//             continue;
+//         }
+        
+//         if (i == 0 || JSON.stringify(winningCombinations[i - 1]) == JSON.stringify(winningCombinations[i])) {
+//             currentCount++;
+//         }
+//         if (currentCount > winningCount) {
+//             winningCount = currentCount;
+//             winningCombination = winningCombinations[i];
+//         }
+//     }
+
+//     if (winningCombination){
+//         console.log('Playing Winning Move');
+//         return winningCombination.pattern[turnCount];
+//     }
+//     else
+//         return '';
+// }
+
+// function computeDrawMove() {
+//     var checkedBoxesSimplified = checkedBoxes.map(b => b.box);
+//     var drawingCombinations = model.filter(m => m.result == 'draw'
+//         && m.winner == -1
+//         && JSON.stringify(m.pattern.slice(0, turnCount)) == JSON.stringify(checkedBoxesSimplified))
+//         .sort();
+    
+//     if(!drawingCombinations)
+//         return '';
+
+//     var drawingCount = 0;
+//     var currentCount = 0;
+//     var drawingCombination;
+
+//     for (var i = 0; i < drawingCombinations.length; i++) {
+//         if (isLosingCombination(drawingCombinations[i]))
+//             continue;
+//         if (i == 0 || JSON.stringify(drawingCombinations[i - 1]) == JSON.stringify(drawingCombinations[i])) {
+//             currentCount++;
+//         }
+//         if (currentCount > drawingCount) {
+//             drawingCount = currentCount;
+//             drawingCombination = drawingCombinations[i];
+//         }
+//     }
+    
+//     if (drawingCombination){
+//         console.log('Playing Drawing Move');
+//         return drawingCombination.pattern[turnCount];
+//     }
+//     else
+//         return '';
+// }
+
+function computeRandomMove() {
+    var remainingMoves = getRemainingMoves();
+    //var movesTried = 0;
+    return remainingMoves[Math.floor(Math.random()*remainingMoves.length)]
+    // var randomMove;
+    // while(true)
+    // {
+    //     console.log('Trying Random Move!');
+    //     randomMove = remainingMoves[Math.floor(Math.random() * remainingMoves.length)];
+    //     remainingMoves = remainingMoves.filter(m => m != randomMove);
+    //     movesTried++;
+    //     if(!isLosingMove(randomMove)){
+    //         console.log('Playing Calculated Random Move');
+    //         return randomMove;
+    //     }
+    //     else if (isLosingMove(randomMove) && movesTried != remainingMoves.length){
+    //         console.log('Playing Any Random Move');
+    //         return getRemainingMoves()[Math.floor(Math.random() * remainingMoves.length)];
+    //     }
+    // }
+}
+
+// function isLosingCombination(combination) {
+//     //debugger;
+
+
+//     return !!model.find(m => m.result == 'win'
+//         && m.winner == (turnCount + 2) % 2 // Check Losing Combination
+//         && JSON.stringify(m.pattern) == JSON.stringify(combination.pattern))
+// }
+
+// function isLosingMove(move){
+//     var playedMoves = checkedBoxes.map(b => b.box);
+//     var appendedPlayedMoves = playedMoves.push(move);
+//     return !!model.find(m => m.result == 'win'
+//                     && m.winner == (turnCount + 2) % 2 // Check Losing Combination
+//                     && JSON.stringify(m.pattern.slice(0, turnCount+1)) == JSON.stringify(appendedPlayedMoves))
+// }
+
+
+
 function getRemainingMoves() {
     var allMoves = ['0-0', '0-1', '0-2',
         '1-0', '1-1', '1-2',
@@ -351,17 +486,42 @@ function getRemainingMoves() {
     return allMoves.filter(m => !playedMoves.find(move => move == m));
 }
 
-function updateModel(result) {
-    showLoader();
-    var winner = result == 'draw' ? -1 : turnCount % 2;
-    model.push({ result: result, winner: winner, pattern: checkedBoxes.map(b => b.box) });
-    localStorage.setItem('model', JSON.stringify(model));
-    hideLoader();
-}
+// function updateModel(result) {
+//     showLoader();
+//     var winner = result == 'draw' ? -1 : turnCount % 2;
+//     var boxes = checkedBoxes.map(b => b.box);
+//     var box1 = transform90deg(boxes.slice());
+//     var box2 = transform90deg(box1.slice());
+//     var box3 = transform90deg(box2.slice());
 
-function getModel(){
-    return localStorage.getItem('model') ? JSON.parse(localStorage.getItem('model')) : [];
-}
+//     model.push({ result: result, winner: winner, pattern: boxes });
+//     model.push({ result: result, winner: winner, pattern: box1 });
+//     model.push({ result: result, winner: winner, pattern: box2 });
+//     model.push({ result: result, winner: winner, pattern: box3 });
+//     localStorage.setItem('model', JSON.stringify(model));
+//     hideLoader();
+// }
+
+// function transform90deg(boxArray) {
+//   boxArray[boxArray.indexOf('0-0')] = '0-2n';
+//   boxArray[boxArray.indexOf('0-1')] = '1-2n';
+//   boxArray[boxArray.indexOf('0-2')] = '2-2n';
+//   boxArray[boxArray.indexOf('1-0')] = '0-1n';
+//   boxArray[boxArray.indexOf('1-1')] = '1-1n';
+//   boxArray[boxArray.indexOf('1-2')] = '2-1n';
+//   boxArray[boxArray.indexOf('2-0')] = '0-0n';
+//   boxArray[boxArray.indexOf('2-1')] = '1-0n';
+//   boxArray[boxArray.indexOf('2-2')] = '2-0n';
+
+//   boxArray = boxArray.map(b => b.replace('n', ''));
+
+//   return boxArray;
+
+// }
+
+// function getModel(){
+//     return localStorage.getItem('model') ? JSON.parse(localStorage.getItem('model')) : [];
+// }
 
 
 function showLoader(){
